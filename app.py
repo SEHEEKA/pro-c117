09881cacfc082c3b68b05614bf400ad632388613
Seheeka <86560213+SEHEEKA@users.noreply.github.com>
2023-04-29 23:12:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for, request, jsonify
+from text_sentiment_prediction import *
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+ 
+@app.route('/predict-emotion', methods=["POST"])
+def predict_emotion():
+    
+    # Get Input Text from POST Request
+    
+    
+    
+        # Response to send if the input_text is undefined
+        
+        
+        # Response to send if the input_text is not undefined
+        
+
+        # Send Response         
+        
+       
+app.run(debug=True)
+
+
+
+    
